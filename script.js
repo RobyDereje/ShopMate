1acb78b323c609cc27bd7360615914f3e61896b2
RobyDereje <154716356+RobyDereje@users.noreply.github.com>
2024-02-23 22:38:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -65,6 +65,12 @@ const clearList = ()=>{
 }
 
 btn.addEventListener('click', processor)
+btn.addEventListener('touchend', function() {
+    processor();
+    btn.classList.add('clicked');
+    setTimeout(function() {
+        btn.classList.remove('clicked');
+    }, 100););
 input.addEventListener('keydown', (e)=>{
     if(e.key === 'Enter'){
         processor()
